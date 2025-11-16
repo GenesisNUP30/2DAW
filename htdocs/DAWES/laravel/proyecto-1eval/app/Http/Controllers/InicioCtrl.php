4754d3448fa56abcfea80adc2ab67b1c2ab2bce4
GenesisNUP30 <gushpiz998@g.educaand.es>
2025-11-16 21:24:43 +0100

    creacion vista/controlador index.blade.php

---
+++ htdocs/DAWES/laravel/proyecto-1eval/app/Http/Controllers/InicioCtrl.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+class InicioCtrl {
+    public function index() 
+    {
+        return view('index');
+    }
+}
--- htdocs/DAWES/laravel/proyecto-1eval/routes/web.php
+++ htdocs/DAWES/laravel/proyecto-1eval/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\InicioCtrl;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::any('/', [InicioCtrl::class, 'index']);
