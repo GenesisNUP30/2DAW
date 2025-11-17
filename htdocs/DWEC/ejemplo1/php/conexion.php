bebf2449898c40bc9db0369cc05e57980de51b00
GenesisNUP30 <gushpiz998@g.educaand.es>
2025-11-17 09:16:10 +0100

    cambios conexion 3

--- htdocs/DWEC/ejemplo1/php/conexion.php
+++ htdocs/DWEC/ejemplo1/php/conexion.php
@@ -5,15 +5,6 @@ $user = "genesisnatalya";
 $password = "jy*fD4@i3CWlgi4h";
 $base_de_datos = "genesisnatalya";
 
-// try {
-//     $conexion = mysqli_connect($host, $user, $password, $base_de_datos);
-//     mysqli_set_charset($conexion, 'utf8');
-// } catch (Exception $ex) {
-//     echo "Ha  fallado la conexión <br>";
-//     echo $ex->getMessage();
-//     exit;
-// }
-
 // Crear conexión
 $conn = new mysqli($host, $usuario, $contraseña, $base_datos);
 
