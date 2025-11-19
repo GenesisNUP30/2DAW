8491aac68de40347104d41d5db5fa5a68bb2cbfb
GenesisNUP30 <gushpiz998@g.educaand.es>
2025-11-20 00:41:02 +0100

    desactivar csrf

--- htdocs/DAWES/laravel/proyecto-1eval/routes/web.php
+++ htdocs/DAWES/laravel/proyecto-1eval/routes/web.php
@@ -5,11 +5,7 @@ use App\Http\Controllers\EliminarCtrl;
 use App\Http\Controllers\InicioCtrl;
 use App\Http\Controllers\ModificarCtrl;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
 
 Route::any('/', [InicioCtrl::class, 'index']);
 
