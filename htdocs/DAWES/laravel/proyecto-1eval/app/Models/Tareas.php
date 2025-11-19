422b052003bcf51b6b1ce4c88a0ddf874c8207ba
GenesisNUP30 <gushpiz998@g.educaand.es>
2025-11-19 22:18:19 +0100

    creacion ElimianrCtrl.php

--- htdocs/DAWES/laravel/proyecto-1eval/app/Models/Tareas.php
+++ htdocs/DAWES/laravel/proyecto-1eval/app/Models/Tareas.php
@@ -79,34 +79,47 @@ class Tareas
     {
         $sql = "SELECT * FROM tareas WHERE id = $id";
         $resultado = $this->bd->query($sql);
-        return $resultado;
+        return $this->bd->LeeRegistro($resultado);
     }
 
     public function actualizarTarea($id, array $datos)
     {
+        $nif_cif = $datos['nifCif'];
+        $persona_contacto = $datos['personaNombre'];
+        $telefono = $datos['telefono'];
+        $correo = $datos['correo'];
+        $descripcion_tarea = $datos['descripcionTarea'];
+        $direccion_tarea = $datos['direccionTarea'];
+        $poblacion = $datos['poblacion'];
+        $codigo_postal = $datos['codigoPostal'];
+        $provincia = $datos['provincia'];
+        $estado_tarea = $datos['estadoTarea'];
+        $operario_encargado = $datos['operarioEncargado'];
+        $fecha_realizacion = $datos['fechaRealizacion'];
+        $anotaciones_anteriores = $datos['anotacionesAnteriores'];
+
         $sql = "UPDATE tareas SET
-            nif_cif = '$datos[nif_cif]',
-            persona_contacto = '$datos[persona_contacto]',
-            telefono = '$datos[telefono]',
-            correo = '$datos[correo]',
-            descripcion_tarea = '$datos[descripcion_tarea]',
-            direccion_tarea = '$datos[direccion_tarea]',
-            poblacion = '$datos[poblacion]',
-            codigo_postal = '$datos[codigo_postal]',
-            provincia = '$datos[provincia]',
-            estado_tarea = '$datos[estado_tarea]',
-            operario_encargado = '$datos[operario_encargado]',
-            fecha_realizacion = '$datos[fecha_realizacion]',
-            anotaciones_anteriores = '$datos[anotaciones_anteriores]'
-        WHERE id = " . (int)$id;
+            nif_cif = '$nif_cif',
+            persona_contacto = '$persona_contacto',
+            telefono = '$telefono',
+            correo = '$correo',
+            descripcion_tarea = '$descripcion_tarea',
+            direccion_tarea = '$direccion_tarea',
+            poblacion = '$poblacion',
+            codigo_postal = '$codigo_postal',
+            provincia = '$provincia',
+            estado_tarea = '$estado_tarea',
+            operario_encargado = '$operario_encargado',
+            fecha_realizacion = '$fecha_realizacion',
+            anotaciones_anteriores = '$anotaciones_anteriores'
+            WHERE id = " . (int)$id;
 
         $this->bd->query($sql);
     }
 
-    public function eliminarTarea($id, array $datos) 
+    public function eliminarTarea($id)
     {
         $sql = "DELETE FROM tareas WHERE id = $id";
         $this->bd->query($sql);
     }
-
 }
