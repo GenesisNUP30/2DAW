549d1ce965053fdb5d3207a94fb72c8349c792e5
GenesisNUP30 <jdirproy@gmail.com>
2025-11-21 09:32:04 +0100

    desactivar csrf

--- htdocs/DAWES/laravel/proyecto-1eval/bootstrap/app.php
+++ htdocs/DAWES/laravel/proyecto-1eval/bootstrap/app.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,13 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+    })
 
-        // Excluir rutas del middleware CSRF
-        $middleware->validateCsrfTokens(except: [
-            '/alta',
-            '/modificar/*',
-            '/eliminar/*',
-        ]);
+    ->withMiddleware(function ($middleware) {
+        $middleware->validateCsrfTokens(except: ['/*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
