0617b03623846bfefbdf6c3d0406a912b455c7d9
GenesisNUP30 <jdirproy@gmail.com>
2026-01-15 08:23:10 +0100

    eliminar carpetas innecesarias

--- htdocs/DAWES/laravel/1Trimestre/Ejemplo_Blade/Ejemplo_Blade/11holaconplantilla.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "blade.php";
-
-// Vamos a usar el patron Singleton para obtener el objeto de Blade
-$blade = TemplateBlade::GetInstance();
-
-echo $blade->render('11holaconplantilla');
-
