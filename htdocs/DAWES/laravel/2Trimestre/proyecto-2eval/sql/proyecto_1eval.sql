1e6b0d74f6f8d321bba9b1c440a5c15ee27ccef9
GenesisNUP30 <jdirproy@gmail.com>
2026-02-13 22:05:21 +0100

    Archivo .sql con base de datos correcta

--- htdocs/DAWES/laravel/2Trimestre/proyecto-2eval/sql/proyecto_1eval.sql
+++ htdocs/DAWES/laravel/2Trimestre/proyecto-2eval/sql/proyecto_1eval.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-01-2026 a las 19:03:59
+-- Tiempo de generación: 13-02-2026 a las 09:52:23
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -46,21 +46,6 @@ INSERT INTO `config_avanzada` (`id`, `provincia_defecto`, `poblacion_defecto`, `
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `login_token`
---
-
-CREATE TABLE `login_token` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(100) NOT NULL,
-  `selector_hash` varchar(255) NOT NULL,
-  `validator_hash` varchar(255) NOT NULL,
-  `expiry_date` datetime NOT NULL,
-  `is_expired` tinyint(4) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tareas`
 --
 
@@ -80,21 +65,20 @@ CREATE TABLE `tareas` (
   `fecha_creacion` datetime DEFAULT current_timestamp(),
   `operario_encargado` varchar(50) DEFAULT NULL,
   `fecha_realizacion` date NOT NULL,
-  `anotaciones_posteriores` text DEFAULT NULL,
-  `fichero_resumen` varchar(255) DEFAULT NULL
+  `anotaciones_posteriores` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tareas`
 --
 
-INSERT INTO `tareas` (`id`, `nif_cif`, `persona_contacto`, `telefono`, `correo`, `direccion`, `poblacion`, `codigo_postal`, `provincia`, `descripcion`, `anotaciones_anteriores`, `estado`, `fecha_creacion`, `operario_encargado`, `fecha_realizacion`, `anotaciones_posteriores`, `fichero_resumen`) VALUES
-(1, '12345678Z', 'Manuel García', '111 222 333', 'manuelgarcia@gmail.com', 'Av Santa Marta', 'Bollullos', '21710', '21', 'Prueba laravel', '', 'R', '2025-11-20 08:20:58', 'Ana María Fernández', '2025-12-31', 'Esto es para probar el rol de administrador. AHORA ESTA MODIFICANDO EL OPERARIO', NULL),
-(2, '12345678Z', 'pepe', '986 562 147', 'prueba2@gmail.com', 'Calle Falsa 123', 'Sevilla', '41014', '41', 'dhftfn', '', 'R', '2025-11-20 08:41:00', 'Lucía Hurtado', '2025-12-04', 'PROBANDO A COMPLETAR OTRA TAREA', NULL),
-(3, '12345678Z', 'laravel5', '986 562 147', 'prueba5@gmail.com', '', 'sevilla', '41014', '41', 't`kgbodrgb', '', 'P', '2025-11-24 22:07:34', 'Carlos Ruiz', '2026-02-20', 'fergverbvetbte', NULL),
-(4, '12345678Z', 'yujuu', '+34 654 874 320', 'prueba2@gmail.com', '', '', '21004', '21', 'foerkpwj', '', 'C', '2025-11-24 23:54:29', 'Carlos Ruiz', '2025-12-24', 'regvergv', NULL),
-(5, '12345678Z', 'No lo se', '123456789', 'adminstrador@gmail.com', 'Calle Picos', 'Moguer', '21800', '21', 'Probando a ver si el administrador puede crear una tarea', '', 'R', '2025-11-27 08:30:27', 'Lucía Hurtado', '2026-01-02', 'subiendo archivo .docx', '5_prueba.docx'),
-(6, '12345678Z', 'Antonio Murillo', '959 78 65 01', 'examen2@gmail.com', 'Calle Espejo, 12', 'Moguer', '21800', '21', 'probando nuevo xampp', 'xampp nuevo instalado', 'P', '2026-01-03 18:28:01', 'Carlos Ruiz', '2026-03-19', 'probando a subir los ficheros despues de instalar xampp de nuevo', '6_tarea_antonio-9-1-26.txt');
+INSERT INTO `tareas` (`id`, `nif_cif`, `persona_contacto`, `telefono`, `correo`, `direccion`, `poblacion`, `codigo_postal`, `provincia`, `descripcion`, `anotaciones_anteriores`, `estado`, `fecha_creacion`, `operario_encargado`, `fecha_realizacion`, `anotaciones_posteriores`) VALUES
+(1, '12345678Z', 'Manuel García', '111 222 333', 'manuelgarcia@gmail.com', 'Av Santa Marta', 'Bollullos', '21710', '21', 'Prueba laravel', '', 'R', '2025-11-20 08:20:58', 'Ana María Fernández', '2025-12-31', 'Esto es para probar el rol de administrador. AHORA ESTA MODIFICANDO EL OPERARIO'),
+(2, '12345678Z', 'pepe', '986 562 147', 'prueba2@gmail.com', 'Calle Falsa 123', 'Sevilla', '41014', '41', 'dhftfn', '', 'R', '2025-11-20 08:41:00', 'Lucía Hurtado', '2025-12-04', 'PROBANDO A COMPLETAR OTRA TAREA'),
+(3, '12345678Z', 'laravel5', '986 562 147', 'prueba5@gmail.com', '', 'sevilla', '41014', '41', 't`kgbodrgb', '', 'B', '2025-11-24 22:07:34', 'Carlos Ruiz', '2025-12-26', 'fergverbvetbte'),
+(4, '12345678Z', 'Prueba 45', '+34 654 874 320', 'prueba2@gmail.com', '', '', '21004', '21', 'foerkpwj', '', 'C', '2025-11-24 23:54:29', 'Carlos Ruiz', '2025-12-24', 'regvergv'),
+(5, '12345678Z', 'No lo se', '123456789', 'adminstrador@gmail.com', 'Calle Picos', 'Moguer', '21800', '21', 'Probando a ver si el administrador puede crear una tarea', '', 'P', '2025-11-27 08:30:27', 'Lucía Hurtado', '2026-01-02', NULL),
+(6, '12345678Z', 'Examen1', '123456789', 'examen1@gmail.com', '', 'Huelva', '21005', '21', 'prueba', '', 'P', '2025-12-05 08:20:29', 'María López', '2026-01-02', NULL);
 
 --
 -- Disparadores `tareas`
@@ -124,12 +108,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `password`, `rol`) VALUES
-(1, 'operario', 'operario', 'operario'),
-(2, 'admin', 'admin123', 'administrador'),
+(1, 'Antonio', 'Antonio123', 'operario'),
+(2, 'Jefe Proyectos', 'Jefe1', 'administrador'),
 (5, 'Genesis', 'genesis', 'administrador'),
-(6, 'jose angel', 'jozeanje', 'administrador'),
-(7, 'antonio', '1234', 'operario'),
-(8, 'prueba', '123', 'operario');
+(6, 'operario1', 'operario1', 'operario'),
+(7, 'Daniel', 'daniel', 'operario');
 
 --
 -- Índices para tablas volcadas
@@ -139,12 +122,6 @@ INSERT INTO `usuarios` (`id`, `usuario`, `password`, `rol`) VALUES
 -- Indices de la tabla `config_avanzada`
 --
 ALTER TABLE `config_avanzada`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `login_token`
---
-ALTER TABLE `login_token`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -168,13 +145,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `config_avanzada`
 --
 ALTER TABLE `config_avanzada`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `login_token`
---
-ALTER TABLE `login_token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `tareas`
@@ -186,7 +157,7 @@ ALTER TABLE `tareas`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
