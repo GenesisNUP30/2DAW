492d62d7f21dce79d40caa1319f5c5b15879aa27
GenesisNUP30 <jdirproy@gmail.com>
2026-03-27 00:36:11 +0100

    Metodo enviar en controlador Factura con captura de errores

--- htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/FacturaController.php
+++ htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/FacturaController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\FacturaMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class FacturaController extends Controller
 {
+    public function enviar($id)
+    {
+        $factura = FacturaMail::with('cuota.cliente')->findOrFail($id);
+
+        if (!$factura->cuota->cliente->correo) {
+            return back()->withErrors('El cliente no tiene email');
+        }
+
+        try {
+            // 2. Enviar el correo
+            Mail::to($factura->cuota->cliente->correo)->send(new FacturaMail($factura));
+
+            // 3. Marcar como enviada (opcional, si tienes ese campo)
+            // $factura->update(['enviada' => true]);
+
+            return back()->with('success', 'Factura enviada correctamente al cliente.');
+        } catch (\Exception $e) {
+            return back()->with('error', 'Error al enviar el correo: ' . $e->getMessage());
+        }
+    }
     /**
      * Display a listing of the resource.
      */
