10ca92f9f740ce8f8baaa7d871a51134af94a8d4
GenesisNUP30 <jdirproy@gmail.com>
2026-03-27 02:22:13 +0100

    Comentarios tareas

--- htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/FacturaController.php
+++ htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/FacturaController.php
@@ -72,6 +72,7 @@ class FacturaController extends Controller
             Mail::to($factura->cuota->cliente->correo)->send(new FacturaMail($factura));
             $factura->update(['enviada' => true]);
 
+            //TODO: No aparece el mensaje de exito (revisar vista)
             return back()->with('success', 'Correo enviado correctamente.');
         } catch (\Exception $e) {
             return back()->with('error', 'Error: ' . $e->getMessage());
