04b927476358559dfd5004f3a4390c1bce4d2ae4
GenesisNUP30 <jdirproy@gmail.com>
2026-03-31 01:31:37 +0200

    Mejora en la vista de listado de empledos

--- htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/UserController.php
+++ htdocs/DAWES/laravel/2Trimestre/app-proyecto2/app/Http/Controllers/UserController.php
@@ -14,16 +14,21 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $user = Auth::user();
-        $itemsPorPagina = ConfigAvanzada::actual()->items_por_pagina ?? 5;
+        $estado = $request->get('estado', '');
 
-        if (!$user->isAdmin()) {
-            abort(403);
+        $query = User::excluyendo($user->id)->ordenadosPorNombre();
+
+        if ($estado === 'activo') {
+            $query->activos();
+        } elseif ($estado === 'baja') {
+            $query->deBaja();
         }
 
-        $empleados = User::excluyendo($user->id)->paginate(3);
+        $empleados = $query->paginate(5)->withQueryString();
+
         return view('empleados.index', compact('empleados'));
     }
 
