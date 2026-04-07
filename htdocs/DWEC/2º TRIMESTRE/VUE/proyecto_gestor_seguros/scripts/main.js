2c16f697f75dcdf6af135567515638475c19fc48
GenesisNUP30 <jdirproy@gmail.com>
2026-04-07 22:08:00 +0200

    Añadir logica de polizas a main

--- htdocs/DWEC/2º TRIMESTRE/VUE/proyecto_gestor_seguros/scripts/main.js
+++ htdocs/DWEC/2º TRIMESTRE/VUE/proyecto_gestor_seguros/scripts/main.js
@@ -1,10 +1,19 @@
 const { createApp } = Vue;
 
 createApp({
-    // Mezclamos las lógicas de los otros archivos
-    mixins: [authLogic, clientesLogic],
-    
-    mounted() {
-        console.log("Aplicación de Seguros lista");
+  // Mezclamos las lógicas de los otros archivos
+  mixins: [authLogic, clientesLogic, polizasLogic],
+  data() {
+    return {
+      vistaActual: "clientes", // Puede ser 'clientes' o 'polizas'
+    };
+  },
+  mounted() {
+    console.log("App montada")
+    // Al arrancar, si hay sesión, cargamos ambos
+    if (this.logueado) {
+      this.cargarClientes();
+      this.cargarPolizas();
     }
-}).mount('#app');
+  },
+}).mount("#app");
