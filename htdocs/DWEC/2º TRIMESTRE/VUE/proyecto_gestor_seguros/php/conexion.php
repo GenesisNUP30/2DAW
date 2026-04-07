203271bab850f7b60c7d6370a11d8ce6777f334b
GenesisNUP30 <jdirproy@gmail.com>
2026-04-07 11:12:49 +0200

    Crear conexion a base de datos

---
+++ htdocs/DWEC/2º TRIMESTRE/VUE/proyecto_gestor_seguros/php/conexion.php
@@ -0,0 +1,17 @@
+<?php
+// Configuración de la base de datos
+$servidor = "localhost";
+$usuario  = "root";
+$password = ""; 
+$basedatos = "gestion_seguros";
+
+// Crear la conexión usando la extensión mysqli
+$conexion = new mysqli($servidor, $usuario, $password, $basedatos);
+
+// Comprobar si hay errores de conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// Establecer el conjunto de caracteres a utf8 para que las tildes y ñ se vean bien
+$conexion->set_charset("utf8mb4");
