6b621ea51adbf7b86cb4dfe4af821b842f733488
GenesisNUP30 <jdirproy@gmail.com>
2026-04-07 16:53:21 +0200

    Comprobar datos del login y mostrar mensaje

--- htdocs/DWEC/2º TRIMESTRE/VUE/proyecto_gestor_seguros/php/login.php
+++ htdocs/DWEC/2º TRIMESTRE/VUE/proyecto_gestor_seguros/php/login.php
@@ -7,35 +7,37 @@ header("Content-Type: application/json; charset=UTF-8");
 include "conexion.php";
 
 // Leemos los datos que vienen de Vue (estilo axios/fetch)
-$datosRecibidos = json_decode(file_get_contents("php://input"));
+$datos = json_decode(file_get_contents("php://input"));
 
 // Inicializamos la respuesta
-$respuesta = [
-    "status" => false,
-    "mensaje" => "Usuario o contraseña incorrectos",
-    "usuario" => null
-];
+$res = ["status" => false, "mensaje" => "Credenciales incorrectas"];
 
-if (!empty($datosRecibidos->usuario) && !empty($datosRecibidos->password)) {
-    $user = $conexion->real_escape_string($datosRecibidos->usuario);
-    $pass = $conexion->real_escape_string($datosRecibidos->password);
+if (!empty($datos->usuario) && !empty($datos->password)) {
+    $user = $conexion->real_escape_string($datos->usuario);
 
     // Consulta para buscar al usuario
-    $sql = "SELECT id, usuario, nombre FROM usuarios WHERE usuario = '$user' AND password = '$pass' LIMIT 1";
-    $resultado = $conexion->query($sql);
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$user' LIMIT 1";
+    $result = $conexion->query($sql);
 
-    if ($resultado->num_rows > 0) {
-        $datosUsuario = $resultado->fetch_assoc();
-        
-        $respuesta["status"] = true;
-        $respuesta["mensaje"] = "Acceso concedido";
-        $respuesta["usuario"] = [
-            "id" => $datosUsuario['id'],
-            "nombre" => $datosUsuario['nombre'],
-            "login" => $datosUsuario['usuario']
-        ];
+    if (!empty($datosRecibidos->usuario) && !empty($datosRecibidos->password)) {
+        $user = $conexion->real_escape_string($datosRecibidos->usuario);
+        $pass = $conexion->real_escape_string($datosRecibidos->password);
+
+        // Consulta para buscar al usuario
+        $sql = "SELECT id, usuario, nombre FROM usuarios WHERE usuario = '$user' AND password = '$pass' LIMIT 1";
+        $resultado = $conexion->query($sql);
+
+        if ($result->num_rows > 0) {
+            $u = $result->fetch_assoc();
+            // VERIFICAMOS EL HASH
+            if (password_verify($datos->password, $u['password'])) {
+                $res = [
+                    "status" => true,
+                    "mensaje" => "¡Hola de nuevo!",
+                    "usuario" => ["id" => $u['id'], "nombre" => $u['nombre']]
+                ];
+            }
+        }
     }
 }
-
-// Enviamos la respuesta a Vue
-echo json_encode($respuesta);
+echo json_encode($res);
