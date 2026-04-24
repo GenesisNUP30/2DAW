bbbfdfd350070ba8b4180ec2e1e99af90b1e30db
GenesisNUP30 <jdirproy@gmail.com>
2026-04-24 22:45:35 +0200

    Preparcion del entorno para usar Inertia

--- htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema3/proyecto/bootstrap/app.php
+++ htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema3/proyecto/bootstrap/app.php
@@ -3,18 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use App\Http\Middleware\RolUsuario;
-use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function ($middleware): void {
         $middleware->alias([
             'role' => RolUsuario::class,
-            ]);
+        ]);
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
