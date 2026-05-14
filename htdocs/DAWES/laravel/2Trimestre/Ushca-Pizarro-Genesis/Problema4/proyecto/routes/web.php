788f1cd1263e327e7efce6ba7b652ca1d2e0d493
GenesisNUP30 <jdirproy@gmail.com>
2026-05-14 19:53:21 +0200

    Pequeño fix en las rutas

--- htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema4/proyecto/routes/web.php
+++ htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema4/proyecto/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\Auth\TwitterController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TareaController;
@@ -18,87 +20,94 @@ Auth::routes();
 | Rutas protegidas (usuarios autenticados)
 |--------------------------------------------------------------------------
 */
+Route::get('/auth/google', [GoogleController::class, 'redirect'])
+    ->name('auth.google');
 
+Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
+
+Route::get('/auth/twitter', [TwitterController::class, 'redirect'])
+    ->name('auth.twitter');
+Route::get('/auth/twitter/callback', [TwitterController::class, 'callback']);
 
 Route::middleware('auth')->group(function () {
-    
+
     Route::get('/', function () {
         if (Auth::user()->isAdmin()) {
             return redirect()->route('home');
-            }
-            return redirect()->route('tareas.index');
-            });
-            /*
+        }
+        return redirect()->route('tareas.index');
+    });
+    /*
             ====== ADMINISTRADOR ======
             */
-            Route::middleware('role:administrador')->group(function () {
-                //TODO: Comprobar la ruta otra vez
-                Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
-                
-                // CRUD TAREAS: crear, leer, modificar, eliminar
-                // Route::get('/tareas', [TareaController::class, 'index'])->name('tareas.index');
-                Route::get('/tareas/crear', [TareaController::class, 'create'])->name('tareas.create');
-                Route::post('/tareas', [TareaController::class, 'store'])->name('tareas.store');
-                
-                Route::get('/tareas/{tarea}/editar', [TareaController::class, 'edit'])->name('tareas.edit');
-                Route::put('/tareas/{tarea}', [TareaController::class, 'update'])->name('tareas.update');
-                
-                Route::get('/tareas/{tarea}/eliminar', [TareaController::class, 'confirmDelete'])->name('tareas.confirmDelete');
-                Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])->name('tareas.destroy');
-                });
-                
-                /*
+    Route::middleware('role:administrador')->group(function () {
+        //TODO: Comprobar la ruta otra vez
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
+
+        // CRUD TAREAS: crear, leer, modificar, eliminar
+        // Route::get('/tareas', [TareaController::class, 'index'])->name('tareas.index');
+        Route::get('/tareas/crear', [TareaController::class, 'create'])->name('tareas.create');
+        Route::post('/tareas', [TareaController::class, 'store'])->name('tareas.store');
+
+        Route::get('/tareas/{tarea}/editar', [TareaController::class, 'edit'])->name('tareas.edit');
+        Route::put('/tareas/{tarea}', [TareaController::class, 'update'])->name('tareas.update');
+
+        Route::get('/tareas/{tarea}/eliminar', [TareaController::class, 'confirmDelete'])->name('tareas.confirmDelete');
+        Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])->name('tareas.destroy');
+    });
+
+    /*
                 ====== OPERARIO ======
                 */
-                Route::middleware(['role:operario'])->group(function () {
-                    // Route::get('/tareas/index', [TareaController::class, 'index'])->name('tareas.index');
-                    Route::get('/tareas/{tarea}/completar', [TareaController::class, 'completeForm'])->name('tareas.completeForm');
-                    Route::post('/tareas/{tarea}/completar', [TareaController::class, 'complete'])->name('tareas.complete');
-                    });
-                    
-                    /*
+    Route::middleware(['role:operario'])->group(function () {
+        // Route::get('/tareas/index', [TareaController::class, 'index'])->name('tareas.index');
+        Route::get('/tareas/{tarea}/completar', [TareaController::class, 'completeForm'])->name('tareas.completeForm');
+        Route::post('/tareas/{tarea}/completar', [TareaController::class, 'complete'])->name('tareas.complete');
+    });
+
+    /*
                     ====== EMPLEADOS ======
                     */
-                    Route::middleware('role:administrador')->group(function () { 
-                        Route::get('/empleados', [UserController::class, 'index'])->name('empleados.index');
+    Route::middleware('role:administrador')->group(function () {
+        Route::get('/empleados', [UserController::class, 'index'])->name('empleados.index');
 
         Route::get('/empleados/crear', [UserController::class, 'create'])->name('empleados.create');
         Route::post('/empleados', [UserController::class, 'store'])->name('empleados.store');
-        
+
         Route::get('/empleados/{empleado}/editar', [UserController::class, 'edit'])->name('empleados.edit');
         Route::put('/empleados/{empleado}', [UserController::class, 'update'])->name('empleados.update');
-        
+
         Route::get('/empleados/{empleado}/eliminar', [UserController::class, 'confirmDelete'])->name('empleados.confirmDelete');
         Route::delete('/empleados/{empleado}', [UserController::class, 'destroy'])->name('empleados.destroy');
-        
+
         Route::get('/empleados/{empleado}/baja', [UserController::class, 'confirmBaja'])->name('empleados.confirmBaja');
         Route::post('/empleados/{empleado}/baja', [UserController::class, 'baja'])->name('empleados.baja');
-        
+
         Route::get('/empleados/{empleado}/alta', [UserController::class, 'confirmAlta'])->name('empleados.confirmAlta');
         Route::post('/empleados/{empleado}/alta', [UserController::class, 'alta'])->name('empleados.alta');
-        });
-        
-        Route::middleware('role:administrador')->group(function () {
-            // CLIENTES
-            Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
-            Route::get('/clientes-js', [ClienteController::class, 'spa'])->name('clientes_js.index');
-            
-            Route::get('/clientes/crear', [ClienteController::class, 'create'])->name('clientes.create');
-            Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
-            
-            Route::get('/clientes/editar/{cliente}', [ClienteController::class, 'edit'])->name('clientes.edit');
-            Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
-            
-            Route::get('/clientes/{cliente}/baja', [ClienteController::class, 'confirmBaja'])->name('clientes.confirmBaja');
-            Route::post('/clientes/{cliente}/baja', [ClienteController::class, 'baja'])->name('clientes.baja');
-            
-            Route::get('/clientes/{cliente}/alta', [ClienteController::class, 'confirmAlta'])->name('clientes.confirmAlta');
-            Route::post('/clientes/{cliente}/alta', [ClienteController::class, 'alta'])->name('clientes.alta');
-            
-            Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
-            });
-            
-            Route::middleware('role:administrador')->group(function () {
+    });
+
+    Route::middleware('role:administrador')->group(function () {
+        // CLIENTES
+        Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+        Route::get('/clientes-js', [ClienteController::class, 'spa'])->name('clientes_js.index');
+
+        Route::get('/clientes/crear', [ClienteController::class, 'create'])->name('clientes.create');
+        Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
+
+        Route::get('/clientes/editar/{cliente}', [ClienteController::class, 'edit'])->name('clientes.edit');
+        Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
+
+        Route::get('/clientes/{cliente}/baja', [ClienteController::class, 'confirmBaja'])->name('clientes.confirmBaja');
+        Route::post('/clientes/{cliente}/baja', [ClienteController::class, 'baja'])->name('clientes.baja');
+
+        Route::get('/clientes/{cliente}/alta', [ClienteController::class, 'confirmAlta'])->name('clientes.confirmAlta');
+        Route::post('/clientes/{cliente}/alta', [ClienteController::class, 'alta'])->name('clientes.alta');
+
+        Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
+    });
+
+    Route::middleware('role:administrador')->group(function () {
         // CUOTAS
         Route::get('/cuotas', [CuotaController::class, 'index'])->name('cuotas.index');
 
