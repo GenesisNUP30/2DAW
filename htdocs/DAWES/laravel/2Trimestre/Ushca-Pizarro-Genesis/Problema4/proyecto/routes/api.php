4aeed825739b99d1caa48e81e82e4c38a079266c
GenesisNUP30 <jdirproy@gmail.com>
2026-05-18 17:29:05 +0200

    Actualizar

--- htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema4/proyecto/routes/api.php
+++ htdocs/DAWES/laravel/2Trimestre/Ushca-Pizarro-Genesis/Problema4/proyecto/routes/api.php
@@ -8,13 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware(['auth', 'role:administrador'])->group(function () {
-
-    Route::apiResource('clientes', ClienteController::class)->names([
+Route::apiResource('clientes', ClienteController::class)->names([
         'index'   => 'api.clientes.index',
         'store'   => 'api.clientes.store',
         'show'    => 'api.clientes.show',
         'update'  => 'api.clientes.update',
         'destroy' => 'api.clientes.destroy',
     ]);
-});
+
